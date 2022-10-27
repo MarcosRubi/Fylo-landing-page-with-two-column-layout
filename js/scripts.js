e4fea0943256f14f85148179cae13a69cc8a2908
MarcosRubi <danielhernandez9980@gmail.com>
2022-10-27 01:12:52 -0600

    Refactorizando validacion de emails

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,57 @@
 window.onload = () => {
+	function validEmail(email, label, containLabel=false) {
+		if (!regexEmail.test(email.value)) {
+			email.style.border = "1px solid red";
+			if (containLabel) {
+				label.innerHTML = "Please check your email";
+				label.style.color = "red";
+			}
+			return;
+		}
+		email.style.border = "hsl(243, 87%, 12%)";
+		if (containLabel) {
+			label.innerHTML = "Enter your email";
+			label.style.color = "hsl(224, 93%, 58%)";
+		}
+	}
+
 	const regexEmail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 	// VALIDACIONES EMAIL HERO
-	const emailHero = document.getElementById("txtEmail");
-	const labelEmail = document.getElementById("labelTxtEmail");
 
 	const formHero = document.querySelector(".form-hero");
-	
+
 	formHero.addEventListener("submit", () => {
-		if (!regexEmail.test(emailHero.value)) {
-			labelEmail.innerHTML = "Please check your email";
-			labelEmail.style.color = "red";
-			emailHero.style.border = "1px solid red";
-			return;
-		}
-		labelEmail.innerHTML = "Enter your email";
-		labelEmail.style.color = "hsl(224, 93%, 58%)";
-		emailHero.style.border = "1px solid hsl(243, 87%, 12%)"; 
+		const labelEmail = document.getElementById("labelTxtEmail");
+		const emailHero = document.getElementById("txtEmail");
+
+		validEmail(emailHero, labelEmail, true);
 	});
 
 	// VALIDACIONES EMAIL SIGN UP
-	const emailSignUp = document.getElementById("txtEmailSignUp");
 
-	const formSignUp= document.querySelector(".signUp__form");
-	
+	const formSignUp = document.querySelector(".signUp__form");
+
 	formSignUp.addEventListener("submit", () => {
-		if (!regexEmail.test(emailSignUp.value)) {
-			emailSignUp.style.border = "1px solid red";
-			return;
-		}
-		emailSignUp.style.border = "hsl(243, 87%, 12%)";
+		const emailSignUp = document.getElementById("txtEmailSignUp");
+
+		validEmail(emailSignUp);
 	});
+
+	// ANIMACIONES
+	const startAnimation = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("visible");
+			}
+		});
+	};
+
+	const observer = new IntersectionObserver(startAnimation, {
+		root: null,
+		rootMargin: "0px",
+		threshold: 0.1,
+	});
+
+	observer.observe(formHero);
 };
